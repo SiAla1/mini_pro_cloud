7243b643e99cd33ffb610de5fd23b9a92a738372
Azaiez Ala <99550777+SiAla1@users.noreply.github.com>
2024-11-30 21:58:59 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 // Start the server
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
