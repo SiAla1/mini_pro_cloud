b531968f8ab495172777f564b901d80a87e4bcc8
Azaiez Ala <99550777+SiAla1@users.noreply.github.com>
2024-11-30 13:26:40 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -10,7 +10,7 @@ const clientRoutes = require('./routes/clientRoutes');
 // Middleware pour gérer les CORS
 app.use(
   cors({
-    origin:*, // Autoriser les requêtes depuis cette origine
+    origin:'*', // Autoriser les requêtes depuis cette origine
     credentials: true, // Permettre l'envoi de cookies ou d'en-têtes autorisés
   })
 );
