4d1e68c86ae50025629ce83adf551c0e7f6ee8c6
ala azaiz <alaazaiz154@gmail.com>
2024-11-30 15:02:40 +0100

    commited

--- back/app.js
+++ back/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 require('dotenv').config(); // Charger les variables d'environnement depuis le fichier .env
 const cors = require('cors');
-// const http=require('http');
+const path = require('path'); // Importer path pour gérer les chemins de fichiers
 
 const app = express();
-// const server= http.createServer(app);
 const clientRoutes = require('./routes/clientRoutes');
 
 // Middleware pour gérer les CORS
 app.use(
   cors({
-    origin:'*', // Autoriser les requêtes depuis cette origine
+    origin: '*', // Autoriser les requêtes depuis toutes les origines
     credentials: true, // Permettre l'envoi de cookies ou d'en-têtes autorisés
   })
 );
@@ -18,8 +17,16 @@ app.use(
 // Middleware pour analyser les corps des requêtes JSON
 app.use(express.json()); // IMPORTANT : Nécessaire pour traiter les requêtes POST et PUT
 
-// Middleware pour les routes
+// Middleware pour les routes API
 app.use('/api', clientRoutes);
+
+// Serve static files from React build folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// For any route that isn't API, send back React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Middleware pour logger les requêtes reçues
 app.use((req, res, next) => {
